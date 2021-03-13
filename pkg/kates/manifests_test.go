bd7b2d6aee5e76affa48f563ab1bb32cc9c2d9cf
med-home-git <shannonmae@aprnhome.com>
2021-03-13 00:44:57 -0500

    Initialize release/v1.8

---
+++ pkg/kates/manifests_test.go
@@ -0,0 +1,104 @@
+package kates
+
+import (
+	"reflect"
+	"testing"
+
+	amb "github.com/datawire/ambassador/pkg/api/getambassador.io/v2"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"k8s.io/api/core/v1"
+)
+
+var svc = `
+# leading comment
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: example-service
+  namespace: foo
+`
+
+func TestByName(t *testing.T) {
+	objs, err := ParseManifests(svc)
+	if err != nil {
+		panic(err)
+	}
+	_ = objs[0].(*v1.Service)
+	svcs := make(map[string]*Service)
+	ByName(objs, svcs)
+	assert.Equal(t, 1, len(svcs))
+	assert.Equal(t, objs[0], svcs["example-service"])
+}
+
+func TestMergeUpdate(t *testing.T) {
+	a := &Unstructured{}
+	a.Object = map[string]interface{}{
+		"apiVersion": "vtest",
+		"kind":       "Foo",
+		"metadata": map[string]interface{}{
+			"name":            "foo",
+			"namespace":       "default",
+			"resourceVersion": "1234",
+			"labels": map[string]interface{}{
+				"foo": "bar",
+			},
+			"annotations": map[string]interface{}{
+				"moo": "arf",
+			},
+		},
+		"spec": map[string]interface{}{
+			"foo": "bar",
+		},
+	}
+
+	b := &Unstructured{}
+	b.Object = map[string]interface{}{
+		"apiVersion": "vtest",
+		"kind":       "Foo",
+		"metadata": map[string]interface{}{
+			"labels": map[string]interface{}{
+				"foo": "bar",
+				"moo": "arf",
+			},
+			"annotations": map[string]interface{}{
+				"foo": "bar",
+				"moo": "arf",
+			},
+		},
+		"spec": map[string]interface{}{
+			"key": "value",
+		},
+	}
+
+	assert.NotEqual(t, a.Object["spec"], b.Object["spec"])
+
+	MergeUpdate(a, b)
+
+	assert.Equal(t, "arf", a.GetLabels()["moo"])
+	assert.Equal(t, "bar", a.GetAnnotations()["foo"])
+	assert.Equal(t, b.Object["spec"], a.Object["spec"])
+}
+
+const mapping = `---
+apiVersion: getambassador.io/v2
+kind: Mapping
+metadata:
+  name:  mapping-name
+spec:
+  prefix: /mapping-prefix/
+  service: http://mapping-service
+`
+
+func TestParseManifestsResultTypes(t *testing.T) {
+	objs, err := ParseManifests(mapping)
+	require.NoError(t, err)
+	require.Equal(t, 1, len(objs))
+
+	m := objs[0]
+	t.Log(m)
+	t.Log(reflect.TypeOf(m))
+	_, ok := m.(*amb.Mapping)
+	require.True(t, ok)
+}
---
+++ python/ambassador/ir/irtracing.py
@@ -0,0 +1,118 @@
+from typing import Optional, TYPE_CHECKING
+
+from .ircluster import IRCluster
+from .irresource import IRResource
+from ..config import Config
+from ..utils import RichStatus
+
+if TYPE_CHECKING:
+    from .ir import IR
+
+
+class IRTracing(IRResource):
+    cluster: Optional[IRCluster]
+    service: str
+    driver: str
+    driver_config: dict
+    tag_headers: list
+    host_rewrite: Optional[str]
+    sampling: dict
+
+    def __init__(self, ir: 'IR', aconf: Config,
+                 rkey: str = "ir.tracing",
+                 kind: str = "ir.tracing",
+                 name: str = "tracing",
+                 namespace: Optional[str] = None,
+                 **kwargs) -> None:
+        del kwargs  # silence unused-variable warning
+
+        super().__init__(
+            ir=ir, aconf=aconf, rkey=rkey, kind=kind, name=name, namespace=namespace
+        )
+        self.cluster = None
+
+    def setup(self, ir: 'IR', aconf: Config) -> bool:
+        # Some of the validations might go away if JSON Schema is doing the validations, but need to check on that
+
+        config_info = aconf.get_config('tracing_configs')
+
+        if not config_info:
+            ir.logger.debug("IRTracing: no tracing config, bailing")
+            # No tracing info. Be done.
+            return False
+
+        configs = config_info.values()
+        number_configs = len(configs)
+        if number_configs != 1:
+            self.post_error(
+                RichStatus.fromError("exactly one TracingService is supported, got {}".format(number_configs),
+                                     module=aconf))
+            return False
+
+        config = list(configs)[0]
+
+        service = config.get('service')
+        if not service:
+            self.post_error(RichStatus.fromError("service field is required in TracingService"))
+            return False
+
+        driver = config.get('driver')
+        if not driver:
+            self.post_error(RichStatus.fromError("driver field is required in TracingService"))
+            return False
+
+        self.namespace = config.get("namespace", self.namespace)
+
+        grpc = False
+        if driver == "lightstep":
+            grpc = True
+
+        if driver == "datadog":
+            driver = "envoy.tracers.datadog"
+
+        driver_config = config.get("config", {})
+        if driver_config:
+            if 'collector_endpoint_version' in driver_config:
+                if not driver_config['collector_endpoint_version'] in ['HTTP_JSON_V1', 'HTTP_JSON', 'HTTP_PROTO']:
+                    self.post_error(RichStatus.fromError("collector_endpoint_version must be one of 'HTTP_JSON_V1, HTTP_JSON, HTTP_PROTO'"))
+                    return False
+
+        # OK, we have a valid config.
+        self.sourced_by(config)
+
+        self.service = service
+        self.driver = driver
+        self.grpc = grpc
+        self.cluster = None
+        self.driver_config = driver_config
+        self.tag_headers = config.get('tag_headers', [])
+        self.sampling = config.get('sampling', {})
+
+        # XXX host_rewrite actually isn't in the schema right now.
+        self.host_rewrite = config.get('host_rewrite', None)
+
+        # Remember that the config references us.
+        self.referenced_by(config)
+
+        return True
+
+    def add_mappings(self, ir: 'IR', aconf: Config):
+        cluster = ir.add_cluster(
+            IRCluster(
+                ir=ir,
+                aconf=aconf,
+                parent_ir_resource=self,
+                location=self.location,
+                service=self.service,
+                host_rewrite=self.get('host_rewrite', None),
+                marker='tracing',
+                grpc=self.grpc
+            )
+        )
+
+        cluster.referenced_by(self)
+        self.cluster = cluster
+
+    def finalize(self):
+        self.ir.logger.debug("tracing cluster envoy name: %s" % self.cluster.envoy_name)
+        self.driver_config['collector_cluster'] = self.cluster.envoy_name
